eeb7409d1fcac1df0bafa08f8eee24fa63b63061
Davi Souza <davimj99@gmail.com>
2026-07-22 18:41:39 -0300

    feat(Carro): aplicando métodos Static no pacote de modificadoresStatic

--- src/onrender/davidev/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/onrender/davidev/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,11 +4,11 @@ import onrender.davidev.maratonajava.javacore.Fmodificadorestatico.dominio.Carro
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite (180);
+
         Carro carro1 = new Carro("BMW", 280);
         Carro carro2 = new Carro("MERCEDES", 275);
         Carro carro3 = new Carro("AUDI", 290);
-
-        Carro.velocidadeLimite = 180;
 
         carro1.imprime();
         carro2.imprime();
