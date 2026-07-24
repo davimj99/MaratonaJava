ffe9c38dc75ed4f53a22d674429bb0356d632b84
Davi Souza <davimj99@gmail.com>
2026-07-24 15:45:03 -0300

    feat(Gassociacao): Treinando leitura de dados pelo console

---
+++ src/onrender/davidev/maratonajava/javacore/Gassociacao/test/LeituraDeTeclado01.java
@@ -0,0 +1,22 @@
+package onrender.davidev.maratonajava.javacore.Gassociacao.test;
+
+import java.util.Scanner;
+
+public class LeituraDeTeclado01 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o nome abaixo: ");
+        String nome = input.nextLine();
+
+        System.out.println("Digite sua idade : ");
+        int idade = input.nextInt();
+
+        System.out.println("Digite M ou F para seu sexo");
+        char sexo = input.next().charAt(0);
+
+        System.out.println("==============");
+        System.out.println("Nome : " + nome);
+        System.out.println("Idade : " + idade);
+        System.out.println("Sexo : " + sexo);
+    }
+}
