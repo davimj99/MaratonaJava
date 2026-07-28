98cf22b603f4c94ac499cbbf369a4b8cd03e6e90
Davi Souza <davimj99@gmail.com>
2026-07-28 20:47:19 -0300

    feat(Isobrescrita): novo tópico, sobrescrita do método toString

---
+++ src/onrender/davidev/maratonajava/javacore/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,11 @@
+package onrender.davidev.maratonajava.javacore.Isobrescrita.test;
+
+import onrender.davidev.maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dragon Ball");
+
+        System.out.println(anime);
+    }
+}
