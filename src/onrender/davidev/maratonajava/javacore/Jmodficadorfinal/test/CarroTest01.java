ea0bdb12f5000f185e84023d8a029a36eeff0cf9
Davi Souza <davimj99@gmail.com>
2026-07-28 21:11:00 -0300

    feat(Jmodificadorfinal): novo pacote, entendendo sobre constantes

---
+++ src/onrender/davidev/maratonajava/javacore/Jmodficadorfinal/test/CarroTest01.java
@@ -0,0 +1,10 @@
+package onrender.davidev.maratonajava.javacore.Jmodficadorfinal.test;
+
+import onrender.davidev.maratonajava.javacore.Jmodficadorfinal.dominio.Carro;
+
+public class CarroTest01 {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
