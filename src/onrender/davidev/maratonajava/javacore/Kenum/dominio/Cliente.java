e20c38490c744109cd2e97692f4f00cfb551cff4
Davi Souza <davimj99@gmail.com>
2026-08-03 14:34:39 -0300

    feat(Kenum): Enumeração em sobrescrita de métodos

--- src/onrender/davidev/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ src/onrender/davidev/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,10 +1,6 @@
 package onrender.davidev.maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
-    public enum TipoPagamento{
-        DEBITO,
-        CREDITO
-    }
 
     private String nome;
     private TipoCliente tipoCliente;
--- src/onrender/davidev/maratonajava/javacore/Kenum/test/ClienteTest01.java
+++ src/onrender/davidev/maratonajava/javacore/Kenum/test/ClienteTest01.java
@@ -1,7 +1,7 @@
 package onrender.davidev.maratonajava.javacore.Kenum.test;
 
 import onrender.davidev.maratonajava.javacore.Kenum.dominio.Cliente;
-import onrender.davidev.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
+import onrender.davidev.maratonajava.javacore.Kenum.dominio.TipoPagamento;
 import onrender.davidev.maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest01 {
@@ -9,8 +9,9 @@ public class ClienteTest01 {
         Cliente cliente1 = new Cliente("David Souza", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
         Cliente cliente2 = new Cliente("Mariana Fernandes", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
-
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
