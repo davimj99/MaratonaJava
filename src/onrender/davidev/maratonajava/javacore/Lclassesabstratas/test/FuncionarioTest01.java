a5cb573c6ecfc9c77112147f1727cb138995ffba
Davi Souza <davimj99@gmail.com>
2026-08-03 18:37:50 -0300

    feat(Lclassesabstratas): inciando conhecimentos em classes abstratas

---
+++ src/onrender/davidev/maratonajava/javacore/Lclassesabstratas/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package onrender.davidev.maratonajava.javacore.Lclassesabstratas.test;
+
+import onrender.davidev.maratonajava.javacore.Lclassesabstratas.dominio.Desenvolvedor;
+import onrender.davidev.maratonajava.javacore.Lclassesabstratas.dominio.Funcionario;
+import onrender.davidev.maratonajava.javacore.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Gerente gerente = new Gerente("Mariana F", 8000);
+        System.out.println(gerente);
+
+        Desenvolvedor desenvolvedor = new Desenvolvedor("David L Souza", 10000);
+        System.out.println(desenvolvedor);
+    }
+}
