ccf3bdce18383110b93266876034324228a6f94f
Davi Souza <davimj99@gmail.com>
2026-08-05 20:55:53 -0300

    feat(Minterfaces): introdução de interfaces

---
+++ src/onrender/davidev/maratonajava/javacore/Minterfaces/dominio/DataLoader.java
@@ -0,0 +1,5 @@
+package onrender.davidev.maratonajava.javacore.Minterfaces.dominio;
+
+public interface DataLoader {
+    void load();
+}
---
+++ src/onrender/davidev/maratonajava/javacore/Minterfaces/test/DataLoaderTest01.java
@@ -0,0 +1,14 @@
+package onrender.davidev.maratonajava.javacore.Minterfaces.test;
+
+import onrender.davidev.maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
+import onrender.davidev.maratonajava.javacore.Minterfaces.dominio.FileLoader;
+
+public class DataLoaderTest01 {
+    public static void main(String[] args) {
+        DatabaseLoader databaseLoader = new DatabaseLoader();
+        databaseLoader.load();
+
+        FileLoader fileLoader = new FileLoader();
+        fileLoader.load();
+    }
+}
