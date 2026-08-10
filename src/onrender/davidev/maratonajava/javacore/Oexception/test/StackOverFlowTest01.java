784fa2c2105a1cfb1d7328fea12871528c31230e
Davi Souza <davimj99@gmail.com>
2026-08-10 15:33:41 -0300

    feat(Oexception): tratamento de exceções, entendo sobre Errors

---
+++ src/onrender/davidev/maratonajava/javacore/Oexception/test/StackOverFlowTest01.java
@@ -0,0 +1,10 @@
+package onrender.davidev.maratonajava.javacore.Oexception.test;
+
+public class StackOverFlowTest01 {
+    public static void main(String[] args) {
+        recursividade();
+    }
+    public static void recursividade(){
+        recursividade();
+    }
+}
