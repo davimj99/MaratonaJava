12eb923543304a707321117c450df9abdb6c2087
Davi Souza <davimj99@gmail.com>
2026-08-10 15:49:17 -0300

    feat(Oexception):entendo sobre os tipos de RuntimeException

--- src/onrender/davidev/maratonajava/javacore/Oexception/error/test/StackOverFlowTest01.java
+++ src/onrender/davidev/maratonajava/javacore/Oexception/error/test/StackOverFlowTest01.java
@@ -1,4 +1,4 @@
-package onrender.davidev.maratonajava.javacore.Oexception.test;
+package onrender.davidev.maratonajava.javacore.Oexception.error.test;
 
 public class StackOverFlowTest01 {
     public static void main(String[] args) {
