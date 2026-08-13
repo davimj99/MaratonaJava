71b7cbcd25f1ad0c61ff5fd64adf272d9c72c9da
Davi Souza <davimj99@gmail.com>
2026-08-13 15:39:50 -0300

    feat(Oexcpetion): lançando exceção unchecked

---
+++ src/onrender/davidev/maratonajava/javacore/Oexception/runtime/test/RuntimeExcpetionTest02.java
@@ -0,0 +1,22 @@
+package onrender.davidev.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExcpetionTest02 {
+    public static void main(String[] args) {
+        divisao(1,0);
+    }
+
+    /**
+     *
+     * @param a
+     * @param b não pode ser zero (usar CNTRL Q para ver javadoc)
+     * @return
+     * @throws IllegalArgumentException caso b seja zero
+     */
+
+    private static int divisao(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento inlegal, não pode ser zero");
+        }
+        return a/b;
+    }
+}
