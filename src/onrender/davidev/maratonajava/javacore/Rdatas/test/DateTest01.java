a8ee3c0e0c4bd8a5f5f481f83e0b3708c777a0c5
Davi Souza <davimj99@gmail.com>
2026-08-24 21:30:22 -0300

    feat(Rdatas): Classes utilitárias entendendo sobre Date

---
+++ src/onrender/davidev/maratonajava/javacore/Rdatas/test/DateTest01.java
@@ -0,0 +1,11 @@
+package onrender.davidev.maratonajava.javacore.Rdatas.test;
+
+import java.util.Date;
+
+public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1787617542399L); // long 10000
+//        System.out.println(date.getTime());
+        System.out.println(date);
+    }
+}
