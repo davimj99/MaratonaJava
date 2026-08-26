9517b71b0977c7914afd0deceb657ccd1f387932
Davi Souza <davimj99@gmail.com>
2026-08-26 19:01:33 -0300

    feat(Sformatacao): entendendo sobre SimpleDateFormat

---
+++ src/onrender/davidev/maratonajava/javacore/Sformatacao/test/SimpleDateFormatTest01.java
@@ -0,0 +1,20 @@
+package onrender.davidev.maratonajava.javacore.Sformatacao.test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SimpleDateFormatTest01 {
+    public static void main(String[] args) {
+            String pattern = "'Amsterdam' dd 'de' MMMM 'de' yyyy";
+//            String pattern = "yyyy.MM.dd G 'at' HH:mm:ss z";
+            SimpleDateFormat sdf = new SimpleDateFormat(pattern);
+            System.out.println(sdf.format(new Date()));
+
+            try{
+                System.out.println(sdf.parse("Amsterdam 26 de agosto de 2026"));
+            } catch (ParseException e){
+                e.printStackTrace();
+            }
+    }
+}
