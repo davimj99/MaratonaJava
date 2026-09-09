5ae7433fe09e6d1c76826e1bd68e44c837d95b26
Davi Souza <davimj99@gmail.com>
2026-09-09 16:45:44 -0300

    feat(Uregex): regex parte 3 - pattern e matcher usando o Range

---
+++ src/onrender/davidev/maratonajava/javacore/Uregex/test/PatternMatcherTest03.java
@@ -0,0 +1,31 @@
+package onrender.davidev.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest03 {
+    public static void main(String[] args) {
+        // \d = todos os digitos
+        // \D = tudo que não por digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = Tudo que for de a-ZA-Z , digitos, _
+        // \W = Tudo que não for incluso no \w
+        // []
+
+//        String regex = "[a-zA-C]";
+        String regex = "0[xX][0-9a-fA-F]";
+//        String texto = "abaaba";
+        String texto2 = "12 0X 0x 0xFFABC 0x109 0x1 ";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+        System.out.println("texto:   " + texto2);
+        System.out.println("indice: 0123456789 ");
+        System.out.println("regex " + regex);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+    }
+}
