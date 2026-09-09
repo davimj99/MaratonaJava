624301a95dab99fc7d6ce3be4686aabacb590152
Davi Souza <davimj99@gmail.com>
2026-09-09 15:21:28 -0300

    feat(Uregex): Regex - Pattern e Matcher usando os Meta caracteres

---
+++ src/onrender/davidev/maratonajava/javacore/Uregex/test/PatternMatcherTest02.java
@@ -0,0 +1,28 @@
+package onrender.davidev.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest02 {
+    public static void main(String[] args) {
+        // \d = todos os digitos
+        // \D = tudo que não por digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = Tudo que for de a-ZA-Z , digitos, _
+        // \W = Tudo que não for incluso no \w
+        String regex = "\\W";
+//        String texto = "abaaba";
+        String texto2 = "abab_2323 adada23#@#dada32";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto2);
+        System.out.println("texto:   " + texto2);
+        System.out.println("indice: 0123456789 ");
+        System.out.println("regex " + regex);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+    }
+}
