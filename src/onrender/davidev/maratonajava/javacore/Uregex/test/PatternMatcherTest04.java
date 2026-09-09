c0604470bfccba65f4b067e4b8aba1f428b05b09
Davi Souza <davimj99@gmail.com>
2026-09-09 16:54:38 -0300

    feat(Uregex): Regex pt 4 - Pattern e Matcher explorando Quantificadores pt 1

---
+++ src/onrender/davidev/maratonajava/javacore/Uregex/test/PatternMatcherTest04.java
@@ -0,0 +1,29 @@
+package onrender.davidev.maratonajava.javacore.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest04 {
+    public static void main(String[] args) {
+        // ? Zero ou uma
+        // * Zero ou mais
+        // + Uma ou mais
+        // {n,m} de n até m
+        // () Agrupar
+        // |
+        // $ fim da linha
+
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+        String texto = "12 0X 0x 0xFFABC 0x10G 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:   " + texto);
+        System.out.println("indice: 0123456789 ");
+        System.out.println("regex " + regex);
+        System.out.println("Posições encontradas");
+
+        while (matcher.find()) {
+            System.out.print(matcher.start() + " " + matcher.group() + "\n");
+        }
+    }
+}
