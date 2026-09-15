82ac86d2eef28509e77bd073391191b1c3133aba
Davi Souza <davimj99@gmail.com>
2026-09-15 17:37:51 -0300

    feat(Vio): IO pt 03 - FileReader

--- src/onrender/davidev/maratonajava/javacore/Vio/test/FileWriterTest01.java
+++ src/onrender/davidev/maratonajava/javacore/Vio/test/FileWriterTest01.java
@@ -9,7 +9,7 @@ package onrender.davidev.maratonajava.javacore.Vio.test;
 import java.io.File;
 import java.io.FileWriter;
 
-public class FileWriter01 {
+public class FileWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true)){
