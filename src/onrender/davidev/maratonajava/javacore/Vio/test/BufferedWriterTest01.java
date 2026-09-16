fb14333b8f950f38766d338079c83f88b03f43de
Davi Souza <davimj99@gmail.com>
2026-09-16 15:27:54 -0300

    feat(Vio): IO pt 05 - BufferedReader

--- src/onrender/davidev/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
+++ src/onrender/davidev/maratonajava/javacore/Vio/test/BufferedWriterTest01.java
@@ -8,10 +8,10 @@ public class BufferedWriterTest01 {
     public static void main(String[] args) {
         File file = new File("file.txt");
         try (FileWriter fw = new FileWriter(file, true);
-             BufferedWriter br = new BufferedWriter(fw)) {
-             br.write("O davidev é brabo ta ficando afiado no javinha");
-             br.newLine();
-             br.flush();
+             BufferedWriter bw = new BufferedWriter(fw)) {
+             bw.write("O davidev é brabo ta ficando afiado no javinha");
+             bw.newLine();
+             bw.flush();
 
         } catch (Exception ex) {
             ex.printStackTrace();
